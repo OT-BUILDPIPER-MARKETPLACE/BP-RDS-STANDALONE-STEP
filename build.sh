98c5e1ccde32ae5b0f697a5139901841ce416804
Sandeep <sandeep@opstree.com>
2022-11-10 11:48:09 +0530

    Added code to run tf

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
 
-echo "I'll create/update RDS whose properties are available"
-# at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
-sleep  $SLEEP_DURATION
+logInfoMessage "I'll create/update RDS whose properties are available at [$WORKSPACE] and have mounted at [$CODEBASE_DIR]"
+sleep  "$SLEEP_DURATION"
+
+cd  "$WORKSPACE"/"${CODEBASE_DIR}"
+
+terraform "$INSTRUCTION"
